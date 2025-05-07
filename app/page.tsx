1fdefa74cf72ddd9ca0405bad768e34ea5183a88
vvhsx <bussines.gabrys@gmail.com>
2025-05-07 03:08:58 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -74,9 +74,9 @@ export default function Home() {
 
               <div className="space-y-8">
                   <div className="space-y-4">
-                      <h3 className="text-xl text-center font-semibold">Automatyzacje</h3>
+                      <h3 className="text-xl font-semibold">Automatyzacje</h3>
 
-                      <div className="flex place-content-center flex-wrap gap-3">
+                      <div className="flex flex-wrap gap-3">
                           <Button variant="outline" asChild className="h-auto py-2 px-4 font-normal">
                               <Link href="#">Boty Discord</Link>
                           </Button>
@@ -96,9 +96,9 @@ export default function Home() {
                   </div>
 
                   <div className="space-y-4">
-                      <h3 className="text-xl text-center font-semibold">Grafika Komputerowa</h3>
+                      <h3 className="text-xl font-semibold">Grafika Komputerowa</h3>
 
-                      <div className="flex place-content-center flex-wrap gap-3">
+                      <div className="flex flex-wrap gap-3">
                           <Button variant="outline" asChild className="h-auto py-2 px-4 font-normal">
                               <Link href="#">Miniaturki na platformę YouTube</Link>
                           </Button>
@@ -130,9 +130,9 @@ export default function Home() {
                   </div>
 
                   <div className="space-y-4">
-                      <h3 className="text-xl text-center font-semibold">UI/UX Design</h3>
+                      <h3 className="text-xl font-semibold">UI/UX Design</h3>
 
-                      <div className="flex place-content-center flex-wrap gap-3">
+                      <div className="flex flex-wrap gap-3">
                           <Button variant="outline" asChild className="h-auto py-2 px-4 font-normal">
                               <Link href="#">Projekty Stron Internetowych</Link>
                           </Button>
