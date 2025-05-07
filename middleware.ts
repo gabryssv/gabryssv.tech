615ee4b0a6910d43477fc7b4ed530e73b79600e9
vvhsx <bussines.gabrys@gmail.com>
2025-05-07 16:59:28 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,27 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
-  
+  const url = request.nextUrl.clone()
+
   // Check if the path is exactly /link
-  if (url.pathname === '/link') {
-    return NextResponse.redirect('https://gabryssv.pl', { status: 301 });
+  if (url.pathname === "/x") {
+    return NextResponse.redirect("https://x.com/gabryssv", { status: 301 })
   }
-  
-  return NextResponse.next();
+
+  // Check if the path is exactly /link2
+  if (url.pathname === "/behance") {
+    return NextResponse.redirect("https://behance.net/gabryssv", { status: 301 })
+  }
+
+  // Check if the path is exactly /link2
+  if (url.pathname === "/be") {
+    return NextResponse.redirect("https://behance.net/gabryssv", { status: 301 })
+  }
+
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/link'],
-};
+  matcher: ["/x", "/behance", "/be"],
+}
