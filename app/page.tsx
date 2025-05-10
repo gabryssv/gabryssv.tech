2f406e8b4f20e4892d19047a008a511081babcd6
vvhsx <bussines.gabrys@gmail.com>
2025-05-10 02:47:34 +0200

    Refine services section header font weight for improved emphasis

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
 
           <section id="services" className="space-y-6">
           <div className="space-x-2 flex">
-                <h2 className="text-5xl tracking-tight font-bold ">Moje <b className="font-almendra">Usługi</b></h2>
+                <h2 className="text-5xl tracking-tight font-semibold">Moje <b className="font-almendra">Usługi</b></h2>
           </div>
 
               <div className="space-y-8">
